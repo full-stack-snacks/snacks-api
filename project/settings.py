f9037e01f7cf733c4030118a06219eb8d222181c
JB-Tellez <jb.tellez@gmail.com>
2022-06-02 12:03:04 -0700

    adds CSRF stuff

--- project/settings.py
+++ project/settings.py
@@ -168,3 +168,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = tuple(env.list("ALLOWED_ORIGINS"))
 CORS_ALLOW_ALL_ORIGINS = env.bool("ALLOW_ALL_ORIGINS")
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://snacks-api-401d18.herokuapp.com'
+]
