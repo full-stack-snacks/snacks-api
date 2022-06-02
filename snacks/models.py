c798781ff0254172ef73887678f495605eec2a36
JB-Tellez <jb.tellez@gmail.com>
2022-06-02 11:46:43 -0700

    initial commit

--- snacks/models.py
+++ snacks/models.py
@@ -2,12 +2,13 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
-class Thing(models.Model):
+class Snack(models.Model):
     name = models.CharField(max_length=256)
-    owner = models.ForeignKey(
+    purchaser = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True
     )
     description = models.TextField(default="", null=True, blank=True)
+    tags = models.JSONField(default=list, null=True)
 
     def __str__(self):
         return self.name
